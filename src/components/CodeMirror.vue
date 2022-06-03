934e1108d563657f979898af8ef3ad675f8ec264
Chase Meadors <c.ed.mead@gmail.com>
2022-06-03 04:25:36 -0600

    Implement image-hiding with badges

--- src/components/CodeMirror.vue
+++ src/components/CodeMirror.vue
@@ -7,9 +7,43 @@ import { EditorState } from '@codemirror/state';
 import { basicSetup } from '@codemirror/basic-setup';
 import { indentWithTab } from '@codemirror/commands';
 import { markdown as langMarkdown } from '@codemirror/lang-markdown';
-import { EditorView, keymap } from '@codemirror/view';
-import { foldService } from '@codemirror/language';
+import { EditorView, keymap, WidgetType, Decoration, ViewPlugin } from '@codemirror/view';
 
+class BadgeWidget extends WidgetType {
+    constructor(text) {
+        super()
+        this.text = text;
+    }
+
+    eq(other) { return this.text == other.text }
+
+    toDOM() {
+        let el = document.createElement('span');
+        el.classList.add('tag');
+        el.classList.add('is-link');
+        el.innerHTML = this.text;
+        return el;
+    }
+}
+
+function hideImages(view) {
+    let decs = [];
+    for (let { from, to } of view.visibleRanges) {
+        let startLine = view.state.doc.lineAt(from);
+        let endLine = view.state.doc.lineAt(to);
+        for (let i = startLine.number; i <= endLine.number; i++) {
+            let line = view.state.doc.line(i);
+            if (line.text.startsWith(IMAGE_LINE_START)) {
+                let d = Decoration.replace({ widget: new BadgeWidget('IMAGE') });
+                decs.push(d.range(line.from, line.to));
+            }
+        }
+    }
+    return Decoration.set(decs);
+}
+
+const IMAGE_LINE_START = `<p class="inline-figure"><img src="`;
+const IMAGE_LINE_END = `"/></p>`;
 
 export default {
     data: () => ({
@@ -46,9 +80,18 @@ export default {
                     this.debounceTimeoutID = window.setTimeout(this.commitDocument.bind(this), this.debounce);
                 }),
                 keymap.of([indentWithTab]),
-                foldService.of((state, lineStart, lineEnd) => {
-                    const beginning = state.sliceDoc(lineStart, lineStart + 4);
-                    if (beginning == '<img') return { from: lineStart, to: lineEnd + 1};
+                ViewPlugin.fromClass(class {
+                    constructor(view) {
+                        this.decorations = hideImages(view);
+                    }
+
+                    update(update) {
+                        if (update.docChanged || update.viewportChanged) {
+                            this.decorations = hideImages(update.view);
+                        }
+                    }
+                }, {
+                    decorations: v => v.decorations,
                 }),
             ],
         });
@@ -64,17 +107,17 @@ export default {
             this.$emit('update:modelValue', this.editorView.state.doc.toString());
         },
         addOrReplaceImageAtCursor(dataURL) {
-            const cursor = this.editorView.state.selection.ranges.map(r => r.head)[0];
-            const line = this.editorView.state.doc.lineAt(cursor);
+            let cursor = this.editorView.state.selection.ranges.map(r => r.head)[0];
+            let line = this.editorView.state.doc.lineAt(cursor);
             this.editorView.dispatch({
-                changes: { from: line.from, to: line.to, insert: `<img src="${ dataURL }"/>\n`},
+                changes: { from: line.from, to: line.to, insert: `<p class="inline-figure"><img src="${ dataURL }"/></p>\n`},
             });
         },
         getImageAtCursor() {
             const cursor = this.editorView.state.selection.ranges.map(r => r.head)[0];
             const line = this.editorView.state.doc.lineAt(cursor);
-            if (!line.text.startsWith('<img')) return null;
-            return line.text.slice(10, line.length - 3);
+            if (!line.text.startsWith(IMAGE_LINE_START)) return null;
+            return line.text.slice(IMAGE_LINE_START.length, line.length - IMAGE_LINE_END.length);
         },
         focus() {
             this.editorView.focus();
