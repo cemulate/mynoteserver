b5f86b33efd88bb8b087e08008cdec38c9509694
Chase Meadors <c.ed.mead@gmail.com>
2022-06-06 23:03:26 -0600

    Fix 100 vh behavior on mobile

--- src/components/App.vue
+++ src/components/App.vue
@@ -199,11 +199,12 @@ export default {
 }
 
 .root {
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
+}
 
     display: flex;
     flex-direction: column;
