fbf4e1de87ffcaabfd2ccd57e0bd682fb52ca4c3
Chase Meadors <c.ed.mead@gmail.com>
2022-06-15 03:38:31 -0600

    Fix static/print page

--- server/print.mjs
+++ server/print.mjs
@@ -10,7 +10,7 @@ function printViewForNote(collection, file, content) {
     <title>${ `${ collection }/${ file }` }</title>
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="stylesheet" href="/app/styles.css">
-    <script src="/mathjax-config.js"></script>
+    <script src="/custom-resource/mathjax-config.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"></script>
 </head>
 
