4117672e8e2d7fa6ae44c5a305640d31c75f9a7c
Chase Meadors <c.ed.mead@gmail.com>
2022-06-21 18:57:07 -0600

    Document

--- server/utils.mjs
+++ server/utils.mjs
@@ -1,4 +1,4 @@
-const APPENDED_START = `
+const MATHJAX_APPENDED_START = `
 // Added by mynoteserver
 window.MathJax = window.MathJax ?? {};
 window.MathJax.tex = window.MathJax.tex ?? {};
@@ -6,9 +6,13 @@ window.MathJax.tex.macros = window.MathJax.tex.macros ?? {};
 `;
 
 function addMacroToMathjaxConfig(mathjaxConfigContent, key, value, arity) {
+    // Instead of attempting to parse/alter the existing file in place (since it could
+    // create window.MathJax in any way!), simply use the strategy in MATHJAX_APPENDED_START
+    // added at the end of the file.
     let newContent = mathjaxConfigContent;
-    let n = mathjaxConfigContent.indexOf(APPENDED_START);
-    if (n < 0) newContent += APPENDED_START;
+    if (mathjaxConfigContent.indexOf(MATHJAX_APPENDED_START) < 0) newContent += MATHJAX_APPENDED_START;
+
+    // This needs to go inside a string literal inside the file, replace \ -> \\.
     let escapedValue = value.replace('\\', '\\\\');
     let newLine = `window.MathJax.tex.macros['${ key }'] = ["${ escapedValue }", ${ arity }];\n`;
     newContent += newLine;
