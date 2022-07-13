5136e619cb81932353b739c094245b685c9fb97f
Chase Meadors <c.ed.mead@gmail.com>
2022-07-12 19:04:57 -0600

    Support multiple prefixes and replacements in hideLines plugin

--- src/lib/codemirror/hide-lines-by-prefix.js
+++ src/lib/codemirror/hide-lines-by-prefix.js
@@ -22,8 +22,10 @@ class BadgeWidget extends WidgetType {
     ignoreEvent() { return false }
 }
 
-function hideLinesByPrefixField(prefix, replacement) {
-    const rep = Decoration.replace({ widget: new BadgeWidget(replacement) });
+function hideLinesByPrefixField(replacementInfo) {
+    const replacements = replacementInfo.map(entry => {
+        return { ...entry, decoration: Decoration.replace({ widget: new BadgeWidget(entry.replacement) })}
+    });
     return StateField.define({
         create() {
             return Decoration.none;
@@ -34,11 +36,16 @@ function hideLinesByPrefixField(prefix, replacement) {
             let newHiddens = [];
             while (pos < tr.newDoc.length) {
                 let line = tr.newDoc.lineAt(pos);
+                // See if there is already a range on this line
                 let duplicate = false;
-                // Set duplicate if there is already a range on this line
                 hiddens.between(line.from, line.to, () => duplicate = true);
-                if (!duplicate && line.text.startsWith(prefix)) {
-                    newHiddens.push(rep.range(line.from, line.to));
+
+                // Skip/don't check if duplicate
+                for (let { prefix, decoration } of duplicate ? [] : replacements) {
+                    if (line.text.startsWith(prefix)) {
+                        newHiddens.push(decoration.range(line.from, line.to));
+                        break;
+                    }
                 }
                 pos = line.to + 1;
             }
