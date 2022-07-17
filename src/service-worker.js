6bd43d1cb50cd3e064dd9c70440ed152331ed566
Chase Meadors <c.ed.mead@gmail.com>
2022-07-17 01:35:58 -0600

    Have service worker cache google fonts urls, and remove mathjax CDN routes.

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,4 +7,4 @@ precacheAndRoute(self.__WB_MANIFEST);
 
 registerRoute(new RegExp('/api/custom-resource/.*'), new NetworkFirst());
 
-registerRoute(new RegExp('https://cdn.jsdelivr.net/npm/mathjax@3/.*'), new CacheFirst());
+registerRoute(new RegExp('https://fonts.googleapis.com/.*'), new CacheFirst());
